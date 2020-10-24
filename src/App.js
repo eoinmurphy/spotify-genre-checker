7d9c3d24b277fd71a5cdc44e7ac951c96db71a86
Eoin Murphy <eoin@hey.com>
2020-10-24 23:37:24 +0100

    Refactored App component, basic auth flow implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      //serverData = {},
+      //filterString = ''
+    }
+  }
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if(!accessToken)
+      return;
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      user: {
+        name: data.display_name
+      }
+    }))
+  }
+  render() {
+    let userName = this.state.user;
+    return (
+      <div className="App">
+        {this.state.user ?
+        <div>
+          <h1>{this.state.user.name}</h1>
+        </div> : <button onClick={() => {
+          window.location = window.location.href.includes('localhost')
+          ? 'http://localhost:8888/login'
+          : 'https://spotify-genre-checker-backend.herokuapp.com/login' } 
+        }
+        >Sign in with Spotify</button>
+        }
+      </div>
+    );
+  }
 }
+
 
 export default App;
