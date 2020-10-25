b2faeadf985b702e3ec5f5a50c6fcb7f3baa52b4
Eoin Murphy <eoin@hey.com>
2020-10-25 17:43:33 +0000

    Form in, first handleSubmit with simple alert and preventDefault()

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import logo from './logo.svg';
-import './App.css';
-import queryString from 'query-string';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import queryString from "query-string";
 
 class App extends Component {
   constructor() {
@@ -9,37 +9,56 @@ class App extends Component {
     this.state = {
       //serverData = {},
       //filterString = ''
-    }
+    };
   }
   componentDidMount() {
     let parsed = queryString.parse(window.location.search);
     let accessToken = parsed.access_token;
-    if(!accessToken)
-      return;
-    fetch('https://api.spotify.com/v1/me', {
-      headers: {'Authorization': 'Bearer ' + accessToken}
-    }).then(response => response.json())
-    .then(data => this.setState({
-      user: {
-        name: data.display_name
-      }
-    }))
+    if (!accessToken) return;
+    fetch("https://api.spotify.com/v1/me", {
+      headers: { Authorization: "Bearer " + accessToken },
+    })
+      .then((response) => response.json())
+      .then((data) =>
+        this.setState({
+          user: {
+            name: data.display_name,
+          },
+        })
+      );
+  }
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert("Alert!");
   }
   render() {
     let userName = this.state.user;
-    console.log(userName); 
+    console.log(userName);
     return (
       <div className="App">
-        {this.state.user ?
-        <div>
-          <h1>{this.state.user.name}</h1>
-        </div> : <button onClick={() => {
-          window.location = window.location.href.includes('localhost')
-          ? 'http://localhost:8888/login'
-          : 'https://spotify-genre-checker-backend.herokuapp.com/login' } 
-        }
-        >Sign in with Spotify</button>
-        }
+        {this.state.user ? (
+          <div>
+            <h1>{this.state.user.name}</h1>
+            <form onSubmit={this.handleSubmit}>
+              <label>
+                URL:
+                <input type="text" />
+              </label>
+              <br />
+              <input type="submit" value="Submit" />
+            </form>
+          </div>
+        ) : (
+          <button
+            onClick={() => {
+              window.location = window.location.href.includes("localhost")
+                ? "http://localhost:8888/login"
+                : "https://spotify-genre-checker-backend.herokuapp.com/login";
+            }}
+          >
+            Sign in with Spotify
+          </button>
+        )}
       </div>
     );
   }
