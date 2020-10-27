2a335f789827b56f46371072fc391ee8c0cfd675
Eoin Murphy <eoin@hey.com>
2020-10-27 22:33:04 +0000

    Adding fonts and first layer of styling

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App extends Component {
         track: {
           artist: response.data.artists[0].name,
           name: response.data.name,
+          artwork: response.data.album.images[0].url,
         },
       });
       /* == Get primary artist == */
@@ -59,52 +60,97 @@ class App extends Component {
     return (
       <div className="App">
         {this.state.user ? (
-          <div>
-            <h1>Username: {this.state.user.name}</h1>
-            <form onSubmit={this.handleSubmit}>
-              <label>
-                URL:
-                <input
-                  type="text"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                />
-              </label>
-              <br />
-              <input type="submit" value="Submit" />
-            </form>
-            {this.state.genres ? (
-              <div>
-                {console.log('showTrack: ' + showTrack)}
-                <h1>Track: {this.state.track.name}</h1>
-                <h1>Artist: {this.state.track.artist}</h1>
-                <p>{this.state.genres}</p>
-                <div>
-                  <ul>
-                  {this.state.genres.map((genre, index) => {
-                    return (
-                      <li key={index}>
-                        {index}: {genre}
-                      </li>
-                    );
-                  })}
-                  </ul>
+          <div className="li-container">
+            <div className="li-header">
+              <h2 className="li-heading">Spotify Genre Checker</h2>
+              <p>
+                <span className="li-subtitle">
+                  But <i>which</i> playlist?
+                </span>
+              </p>
+            </div>
+            <div className="li-body">
+              <div className="li-body-left">
+                <h2 className="li-how-to">How do I use this?</h2>
+                <ul className="li-how-to-steps">
+                  <li>Go to Spotify</li>
+                  <li>Tap or right click a track</li>
+                  <li>Copy the URL</li>
+                  <li>Paste below</li>
+                  <li>Et voila</li>
+                </ul>
+                <div className="li-form">
+                  <form onSubmit={this.handleSubmit}>
+                    <label for="url" className="li-form-label">
+                      Enter URL:
+                    </label>
+                    <br />
+                    <input
+                      type="text"
+                      name="url"
+                      className="li-form-input"
+                      placeholder="e.g. https://spotify.com/track/..."
+                      value={this.state.value}
+                      onChange={this.handleChange}
+                    />
+                    <br />
+                    {/* <input type="submit" value="" /> */}
+                  </form>
+                </div>
+                <div className="li-history">
+                  <h2 className="li-history-heading">History</h2>
+                  <p>
+                    Coming soon! <i>(maybe)</i>
+                  </p>
                 </div>
               </div>
-            ) : (
-              <span>&#8203;</span>
-            )}
+              {/* TODO: Float right correctly */}
+              <div className="li-body-right">
+                {this.state.genres ? (
+                  <div>
+                    {console.log("showTrack: " + showTrack)}
+                    <h1>Track: {this.state.track.name}</h1>
+                    <h1>Artist: {this.state.track.artist}</h1>
+                    <div>
+                      <h1>Genres:</h1>
+                      <ul>
+                        {this.state.genres.map((genre, index) => {
+                          return (
+                            <li key={index}>
+                              {index}: {genre}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      <h1>Artwork</h1>
+                      <img src={this.state.track.artwork} />
+                    </div>
+                  </div>
+                ) : (
+                  <span>&#8203;</span>
+                )}
+              </div>
+            </div>
           </div>
         ) : (
-          <button
-            onClick={() => {
-              window.location = window.location.href.includes("localhost")
-                ? "http://localhost:8888/login"
-                : "https://spotify-genre-checker-backend.herokuapp.com/login";
-            }}
-          >
-            Sign in with Spotify
-          </button>
+          <div className="lo-container">
+            <h1 className="lo-heading">Spotify Genre Checker</h1>
+            <p>
+              <span className="lo-subtitle">
+                But <i>which</i> playlist?
+              </span>
+            </p>
+            <button
+              className="lo-btn"
+              onClick={() => {
+                window.location = window.location.href.includes("localhost")
+                  ? "http://localhost:8888/login"
+                  : "https://spotify-genre-checker-backend.herokuapp.com/login";
+              }}
+            >
+              Sign in with Spotify
+            </button>
+          </div>
         )}
       </div>
     );
